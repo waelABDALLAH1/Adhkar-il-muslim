1af427b9d612c51de0b483132b5997113f0afa1d
wael <wael.abdallah@etudiant-enit.utm.tn>
2023-11-30 23:02:06 +0100

    hata il 00-

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,7 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../models/section_model.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<SectionModel> sections=[];
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadSections();
+
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -53,4 +75,22 @@ class HomeScreen extends StatelessWidget {
           ),
         )));
   }
+
+
+  loadSections() async {
+    DefaultAssetBundle.of(context)
+        .loadString("assets/database/Section_db.json")
+        .then((data) {
+      var response = json.decode(data);
+
+      response.forEach((section) {
+        SectionModel _section = SectionModel.fromJson(section);
+        sections.add(_section);
+      });
+      setState(() {});
+    }).catchError((error) {
+      print(error);
+    });
 }
+}
+
