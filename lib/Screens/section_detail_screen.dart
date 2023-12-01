6fc4d62f3d814208f382162d21ac269b8c919754
wael <wael.abdallah@etudiant-enit.utm.tn>
2023-12-01 12:27:59 +0100

    hata il 0010 finaaaallllllll hmdlhhhh

--- lib/Screens/section_detail_screen.dart
+++ lib/Screens/section_detail_screen.dart
@@ -1,16 +1,88 @@
+import 'package:adhkar/models/section_detail_model.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-
-class SectionDetailScreen extends StatelessWidget {
+class SectionDetailScreen extends StatefulWidget {
   final int id;
-  const SectionDetailScreen({super.key , required this.id });
+  final String title;
+
+  const SectionDetailScreen({super.key, required this.id ,required this.title});
+
+  @override
+  State<SectionDetailScreen> createState() => _SectionDetailScreenState();
+}
+
+class _SectionDetailScreenState extends State<SectionDetailScreen> {
+  List<SectionDetailModel> sectiondetail = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadSectionDetail();
+  }
+  @override
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text("$id")),
+      appBar: AppBar(
+        title: Text("${widget.title}"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView.separated(
+          physics: BouncingScrollPhysics(),
 
+            itemBuilder: (context , index ) {
+              return Container(
+
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3)
+                    )
+                  ]
+                ),
+
+                child: ListTile(
+                  title: Text("${sectiondetail[index].reference}" , textDirection: TextDirection.rtl),
+                  subtitle: Text("${sectiondetail[index].content}" , textDirection: TextDirection.rtl , style:  TextStyle (
+                    fontSize: 18
+                  )
+                    ,),
+                ),
+              );
+            },
+            separatorBuilder: (context , index ) => Divider(height: 2,color: Colors.grey.withOpacity(.8),),
+            itemCount: sectiondetail.length),
+      ),
     );
+  }
+
+  loadSectionDetail() async {
+    sectiondetail =[];
+    DefaultAssetBundle.of(context)
+        .loadString("assets/database/Section_details_db.json")
+        .then((data) {
+      var response = json.decode(data);
+
+      response.forEach((section) {
+        SectionDetailModel _detailsection =
+            SectionDetailModel.fromJson(section);
+        if( _detailsection.sectionId == widget.id){
+          sectiondetail.add(_detailsection);
+        }
+
+      });
+      setState(() {});
+    }).catchError((error) {
+      print(error);
+    });
   }
 }
---
+++ lib/models/section_detail_model.dart
@@ -0,0 +1,22 @@
+class SectionDetailModel {
+  int? sectionId;
+
+  String? count;
+
+  String? description;
+
+  String? reference;
+
+  String? content;
+
+  SectionDetailModel(this.sectionId, this.count, this.description, this.reference,
+      this.content);
+
+  SectionDetailModel.fromJson(Map<String, dynamic> json) {
+    sectionId = json["section_id"];
+    count = json["count"];
+    description = json["description"];
+    reference = json["reference"];
+    content = json["content"];
+  }
+}
